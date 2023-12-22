1187f680b151bf15fffd74e0e44c5724696bc95d
Pochechuev <trakturus@list.ru>
2023-12-22 13:25:07 +0300

    wrote Sem1_MassOne

--- Seminars/Sem1/MassOne.java
+++ Seminars/Sem1/MassOne.java
@@ -5,6 +5,16 @@ package Seminars.Sem1;
 
 public class MassOne {
     public static void main(String[] args) {
-        
+        int[] arr = { 1, 1, 0, 1, 1, 1 ,1 };
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                count++;
+            } else {
+                count = 0;
+            }
+
+        }
+    System.out.printf("Кол-во подряд идущих единиц в массиве равно: %d", count);
     }
 }
