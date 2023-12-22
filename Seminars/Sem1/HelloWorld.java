cf7d020dfb7947db3562f35f1889f8ab1be6af39
Pochechuev <trakturus@list.ru>
2023-12-22 12:23:41 +0300

    sdf

---
+++ Seminars/Sem1/HelloWorld.java
@@ -0,0 +1,13 @@
+// 📌 Настроить проект, вывести в консоль “Hello world!”.
+// 📌 Вывести в консоль системные дату и время.
+
+package Seminars.Sem1;
+
+import java.time.LocalDateTime;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println(LocalDateTime.now());
+    }
+}
--- Seminars/Sem1/Hello_world.java
+++
@@ -1,7 +0,0 @@
-package Seminars.Sem1;
-
-public class Hello_world {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
