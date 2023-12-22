19917ba795ee346795e401ccffdc376f4e1f8269
Pochechuev <trakturus@list.ru>
2023-12-22 13:48:27 +0300

    wrote Sem1_MassOne

--- Seminars/Sem1/MassOne.java
+++ Seminars/Sem1/MassOne.java
@@ -5,16 +5,26 @@ package Seminars.Sem1;
 
 public class MassOne {
     public static void main(String[] args) {
-        int[] arr = { 1, 1, 0, 1, 1, 1 ,1 };
+        int[] arr = { 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1 };
+        int countMax = getCountMax(arr);
+
+        System.out.printf("Максимальное кол-во подряд идущих единиц в массиве равно: %d", countMax);
+    }
+
+    private static int getCountMax(int[] arr) {
         int count = 0;
+        int countMax = 0;
+
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 1) {
                 count++;
+                countMax = countMax > count ? countMax : count;
             } else {
                 count = 0;
+
             }
 
         }
-    System.out.printf("Кол-во подряд идущих единиц в массиве равно: %d", count);
+        return countMax;
     }
 }
