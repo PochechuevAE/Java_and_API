027c80d1bab021480433a74a2d9f20ea32d198df
Pochechuev <trakturus@list.ru>
2023-12-22 11:27:29 +0300

    add Lection1_functions

---
+++ Lections/Lection1/functions.java
@@ -0,0 +1,12 @@
+package Lections.Lection1;
+
+public class functions {
+    public static void main(String[] args) {
+        sayHi();
+        
+    }
+    
+    static public void sayHi(){
+            System.out.println("hi");
+        }
+}
