08724d9500182c6578e7ce7928c2e58bb2c22d6c
Pochechuev <trakturus@list.ru>
2023-12-22 13:08:14 +0300

    add Sem1_Name

---
+++ Seminars/Sem1/Name.java
@@ -0,0 +1,18 @@
+// 📌 Написать программу, которая запросит пользователя ввести
+// <Имя> в консоли.
+// 📌 Получит введенную строку и выведет в консоль сообщение
+// “Привет, <Имя>!”
+
+package Seminars.Sem1;
+
+import java.util.Scanner;
+
+public class Name {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in, "ibm866");
+        System.out.println("Введите своё имя: ");
+        String text = scanner.nextLine();
+        System.out.printf("Привет, %s", text);
+        scanner.close();
+    }
+}
