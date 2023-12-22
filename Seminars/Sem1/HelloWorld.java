e955a5bff9824ab99b43e6da12e22b598b012266
Pochechuev <trakturus@list.ru>
2023-12-22 13:04:09 +0300

    фыав

--- Seminars/Sem1/HelloWorld.java
+++ Seminars/Sem1/HelloWorld.java
@@ -4,10 +4,18 @@
 package Seminars.Sem1;
 
 import java.time.LocalDateTime;
+import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println(LocalDateTime.now());
+
+        Scanner scanner = new Scanner(System.in, "ibm866"); // Кодировка для ввода русского
+        String text = scanner.nextLine();
+        System.out.println(text);
+        scanner.close();
+
+System.out.println();
     }
 }
