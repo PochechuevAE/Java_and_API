d9aab87b7fb1902ac878440a8d145baae87f61d1
Pochechuev <trakturus@list.ru>
2024-01-11 20:58:50 +0300

    уып

--- Seminars/Sem4/CompareLists.java
+++ Seminars/Sem4/CompareLists.java
@@ -1,100 +1,100 @@
-// Задание №0
-// 1) Замерьте время, за которое в ArrayList добавятся 10000 элементов.
-// 2) Замерьте время, за которое в LinkedList добавятся 10000 элементов.
-// Сравните с предыдущим.
+// // Задание №0
+// // 1) Замерьте время, за которое в ArrayList добавятся 10000 элементов.
+// // 2) Замерьте время, за которое в LinkedList добавятся 10000 элементов.
+// // Сравните с предыдущим.
 
-package Seminars.Sem4;
+// package Seminars.Sem4;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+// import java.util.ArrayList;
+// import java.util.LinkedList;
+// import java.util.List;
 
-public class CompareLists {
-    public static void main(String[] args) {
-        int size = 200_000;
+// public class CompareLists {
+//     public static void main(String[] args) {
+//         int size = 200_000;
 
-        System.out.println("Добавление элементов в начало списка: ");
-        long start = System.currentTimeMillis();
-        fillArrayListFirst(size);
-        System.out.println("ArrayList: " + (System.currentTimeMillis() - start));
+//         System.out.println("Добавление элементов в начало списка: ");
+//         long start = System.currentTimeMillis();
+//         fillArrayListFirst(size);
+//         System.out.println("ArrayList: " + (System.currentTimeMillis() - start));
 
-        start = System.currentTimeMillis();
-        fillLinkedListFirst(size);
-        System.out.println("LinkedList: " + (System.currentTimeMillis() - start));
-        System.out.println();
+//         start = System.currentTimeMillis();
+//         fillLinkedListFirst(size);
+//         System.out.println("LinkedList: " + (System.currentTimeMillis() - start));
+//         System.out.println();
 
-        System.out.println("Добавление элементов в конец списка: ");
-        start = System.currentTimeMillis();
-        fillArrayListLast(size);
-        System.out.println("ArrayList: " + (System.currentTimeMillis() - start));
+//         System.out.println("Добавление элементов в конец списка: ");
+//         start = System.currentTimeMillis();
+//         fillArrayListLast(size);
+//         System.out.println("ArrayList: " + (System.currentTimeMillis() - start));
 
-        start = System.currentTimeMillis();
-        fillLinkedListLast(size);
-        System.out.println("LinkedList: " + (System.currentTimeMillis() - start));
-        System.out.println();
+//         start = System.currentTimeMillis();
+//         fillLinkedListLast(size);
+//         System.out.println("LinkedList: " + (System.currentTimeMillis() - start));
+//         System.out.println();
 
-        System.out.println("Добавление элементов в середину списка: ");
-        start = System.currentTimeMillis();
-        fillArrayListMedium(size);
-        System.out.println("ArrayList: " + (System.currentTimeMillis() - start));
+//         System.out.println("Добавление элементов в середину списка: ");
+//         start = System.currentTimeMillis();
+//         fillArrayListMedium(size);
+//         System.out.println("ArrayList: " + (System.currentTimeMillis() - start));
 
-        start = System.currentTimeMillis();
-        fillLinkedListMedium(size);
-        System.out.println("LinkedList: " + (System.currentTimeMillis() - start));
+//         start = System.currentTimeMillis();
+//         fillLinkedListMedium(size);
+//         System.out.println("LinkedList: " + (System.currentTimeMillis() - start));
 
-    }
+//     }
 
-    public static List<Integer> fillArrayListFirst(int size) {
-        List<Integer> arrayList = new ArrayList<>();
+//     public static List<Integer> fillArrayListFirst(int size) {
+//         List<Integer> arrayList = new ArrayList<>();
 
-        for (int i = 0; i < size; i++) {
-            arrayList.addFirst(i);
-        }
-        return arrayList;
-    }
+//         for (int i = 0; i < size; i++) {
+//             arrayList.addFirst(i);
+//         }
+//         return arrayList;
+//     }
 
-    public static List<Integer> fillLinkedListFirst(int size) {
-        List<Integer> linkedList = new LinkedList<>();
+//     public static List<Integer> fillLinkedListFirst(int size) {
+//         List<Integer> linkedList = new LinkedList<>();
 
-        for (int i = 0; i < size; i++) {
-            linkedList.addFirst(i);
-        }
-        return linkedList;
-    }
+//         for (int i = 0; i < size; i++) {
+//             linkedList.addFirst(i);
+//         }
+//         return linkedList;
+//     }
 
-    public static List<Integer> fillArrayListLast(int size) {
-        List<Integer> arrayList = new ArrayList<>();
+//     public static List<Integer> fillArrayListLast(int size) {
+//         List<Integer> arrayList = new ArrayList<>();
 
-        for (int i = 0; i < size; i++) {
-            arrayList.addLast(i);
-        }
-        return arrayList;
-    }
+//         for (int i = 0; i < size; i++) {
+//             arrayList.addLast(i);
+//         }
+//         return arrayList;
+//     }
 
-    public static List<Integer> fillLinkedListLast(int size) {
-        List<Integer> linkedList = new LinkedList<>();
+//     public static List<Integer> fillLinkedListLast(int size) {
+//         List<Integer> linkedList = new LinkedList<>();
 
-        for (int i = 0; i < size; i++) {
-            linkedList.addLast(i);
-        }
-        return linkedList;
-    }
+//         for (int i = 0; i < size; i++) {
+//             linkedList.addLast(i);
+//         }
+//         return linkedList;
+//     }
 
-    public static List<Integer> fillArrayListMedium(int size) {
-        List<Integer> arrayList = new ArrayList<>();
+//     public static List<Integer> fillArrayListMedium(int size) {
+//         List<Integer> arrayList = new ArrayList<>();
 
-        for (int i = 0; i < size; i++) {
-            arrayList.add(arrayList.size()/2, i);
-        }
-        return arrayList;
-    }
+//         for (int i = 0; i < size; i++) {
+//             arrayList.add(arrayList.size()/2, i);
+//         }
+//         return arrayList;
+//     }
 
-    public static List<Integer> fillLinkedListMedium(int size) {
-        List<Integer> linkedList = new LinkedList<>();
+//     public static List<Integer> fillLinkedListMedium(int size) {
+//         List<Integer> linkedList = new LinkedList<>();
 
-        for (int i = 0; i < size; i++) {
-            linkedList.add(linkedList.size()/2, i);
-        }
-        return linkedList;
-    }
-}
+//         for (int i = 0; i < size; i++) {
+//             linkedList.add(linkedList.size()/2, i);
+//         }
+//         return linkedList;
+//     }
+// }
