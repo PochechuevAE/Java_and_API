f9a1b5421e12e558c856d142d418485e2dcb4c98
Pochechuev <trakturus@list.ru>
2024-01-18 20:18:03 +0300

    sdgf

---
+++ Seminars/Sem6/Cat.java
@@ -0,0 +1,23 @@
+public class Cat {
+    private String name;
+    private String collor;
+    private int age;
+
+    public Cat(String name, String collor, int age) {
+        this.name = name;
+        this.collor = collor;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCollor() {
+        return collor;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
---
+++ Seminars/Sem6/CatDB.java
@@ -0,0 +1,5 @@
+public class CatDB {
+    public static void main(String[] args) {
+        
+    }
+}
