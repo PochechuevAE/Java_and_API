51c419a8d7309f40d267292b89ae77d35849cf66
Pochechuev <trakturus@list.ru>
2024-01-18 20:46:02 +0300

    Sfa

--- Seminars/Sem6/Cat.java
+++ Seminars/Sem6/Cat.java
@@ -1,11 +1,13 @@
+import java.util.Objects;
+
 public class Cat {
     private String name;
-    private String collor;
+    private String color;
     private int age;
 
-    public Cat(String name, String collor, int age) {
+    public Cat(String name, String color, int age) {
         this.name = name;
-        this.collor = collor;
+        this.color = color;
         this.age = age;
     }
 
@@ -13,11 +15,45 @@ public class Cat {
         return name;
     }
 
-    public String getCollor() {
-        return collor;
+    public String getColor() {
+        return color;
     }
 
     public int getAge() {
         return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Имя: " + name.toString() + ", Окрас: " + color.toString() + ", Возраст: " + age;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj){
+            return true;
+        }
+        if ((obj == null) || (getClass() != obj.getClass())){
+            return false;
+        }
+        Cat cat = (Cat) obj;
+        return name.equals(cat.name) && color.equals(cat.color) && age == cat.age;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, color, age);
     }
 }
