5444e3a9f84c46fa61e7b932a735bd809e1a82e6
PochechuevAE <trakturus@list.ru>
2024-01-20 20:21:30 +0300

    wrote Nouts and add NoutsDB and ReadME

---
+++ HW/HW6/Nouts.java
@@ -0,0 +1,73 @@
+package HW.HW6;
+
+public class Nouts {
+    private String name;
+    private int price;
+    private String producer;
+    private String operatingSystem;
+    private int ram;
+    private int hardDiskSpace;
+    private String color;
+
+    public Nouts(String name, int price, String producer, String operatingSystem, int ram, int hardDiskSpace,
+            String color) {
+        this.name = name;
+        this.price = price;
+        this.producer = producer;
+        this.operatingSystem = operatingSystem;
+        this.ram = ram;
+        this.hardDiskSpace = hardDiskSpace;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public String getOperatingSystem() {
+        return operatingSystem;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public int getHardDiskSpace() {
+        return hardDiskSpace;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
